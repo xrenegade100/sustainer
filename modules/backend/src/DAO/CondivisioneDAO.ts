f05afaa568b47e51ecc433b98d943a3c62c40310
ldamato17 <l.damato17@studenti.unisa.it>
2023-12-25 09:00:24 +0100

    build(server): aggiungo campi alla query in CondivisioneDAO

--- modules/backend/src/DAO/CondivisioneDAO.ts
+++ modules/backend/src/DAO/CondivisioneDAO.ts
@@ -11,7 +11,15 @@ connessioneDB.query(
 
     // Accesso ai valori nei risultati
     results.forEach((row) => {
-      console.log('Identificativo modello:', row.id_condivisione);
+      console.log('Identificativo condivisione:', row.id_condivisione);
+      console.log('Identificativo utente:', row.id_utente);
+      console.log('Identificativo modello:', row.id_modello);
+      console.log('Titolo:', row.titolo);
+      console.log('Descrizione:', row.descrizione);
+      console.log('Data Condivisione:', row.data_condivisione);
+      console.log('Permesso download:', row.permesso_download);
+      console.log('Hashtag: #', row.hashtag);      
+
     });
   },
 );
