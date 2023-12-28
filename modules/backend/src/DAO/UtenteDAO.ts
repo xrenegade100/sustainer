46aa481f54b6c5ec72f7d109096b32974c28ccba
Fatted <a.damato73@studenti.unisa.it>
2023-12-28 11:29:14 +0100

    fix(backend): miglioro il metodo SetUtentiRegistrazione

--- modules/backend/src/DAO/UtenteDAO.ts
+++ modules/backend/src/DAO/UtenteDAO.ts
@@ -20,12 +20,31 @@ const getUtenteByMail = async (email: string) => {
   return user[0][0];
 };
 
-const setUtentiRegistrazione = async (nome: string, cognome: string, email: string, password: string) => {
+/* 
+metodo che inserisce un utente nel database e gli assegna un piano free (preso dalla tabella piano con tipo free) 
+e aggiunge un acquisto (nella tabella acquisto) con id_utente, id_piano e data_acquisto
+*/
+const setUtentiRegistrazione = async (
+  nome: string,
+  cognome: string,
+  email: string,
+  password: string,
+) => {
   const connection = await db();
   const user = await connection.query(
     'INSERT INTO utente (nome, cognome, email, password) VALUES (?, ?, ?, ?)',
     [nome, cognome, email, password],
   );
+
+  const piano = await connection.query(
+    'SELECT id_piano FROM piano WHERE tipo = Free',
+  );
+
+  const acquisto = await connection.query(
+    'INSERT INTO acquisto (id_utente, id_piano, data_acquisto) VALUES (?, ?,?)',
+    [user[0].id_utente, piano[0].id_piano, new Date()],
+  );
+
   return user;
 };
 
