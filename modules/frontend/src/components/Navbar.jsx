fc22ea6b11a7afea3ea1481667037563b08a58c1
GabrielMatteo <matteobalasa@gmail.com>
2023-12-31 12:26:34 +0100

    feat(frontend): aggiunta navbar gestitata dalla sessione e logout

--- modules/frontend/src/components/Navbar.jsx
+++ modules/frontend/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Navbar.css'; // Importa il file di stile CSS per la navbar
+import '../styles/Navbar.css';
 
 const Navbar = () => {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
 
   const handleAvatarClick = () => {
     console.log('Avatar clicked');
@@ -12,15 +13,59 @@ const Navbar = () => {
 
   const handleDropdownKeyDown = (event) => {
     if (event.key === 'Enter') {
-      // Aggiungi la logica che desideri eseguire quando viene premuto Invio
       console.log('Enter key pressed');
     }
   };
+
+  const handleLogout = async () => {
+    console.error('Entro in navbar logout');
+    try {
+      const response = await fetch('http://localhost:5000/logout', {
+        method: 'POST',
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        setIsUserAuthenticated(false);
+        // Altri passaggi di pulizia, reindirizzamento, ecc., se necessario
+      } else {
+        console.error('Errore durante il logout:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Errore durante il logout:', error);
+    }
+  };
+
+  useEffect(() => {
+    async function funzioneVerifica() {
+      try {
+        const verifica = await fetch('http://localhost:5000/verificaLogin', {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          credentials: 'include',
+        });
+        const response = await verifica.json();
+
+        if (response.user) {
+          setIsUserAuthenticated(true);
+        } else {
+          setIsUserAuthenticated(false);
+        }
+      } catch (error) {
+        console.error('Errore durante la verifica del login:', error);
+      }
+    }
+
+    funzioneVerifica();
+  }, []); // L'array delle dipendenze è vuoto in quanto non si basa su alcuno stato o proprietà
 
   return (
     <nav className="navbar">
       <ul>
         <img className="logo" src="/Logo.png" alt="" />
+        {/* ... (Altri elementi della navbar) */}
         <li
           className="dropdown-li"
           role="button"
@@ -29,13 +74,30 @@ const Navbar = () => {
           onKeyDown={handleDropdownKeyDown}
         >
           <img className="img-avatar" src="/Avatar.png" alt="" />
-
           {isDropdownVisible && (
             <div className="dropdown-content">
-              {/* Contenuto del menù a tendina */}
-              <Link to="/login" className="text-black">
-                Accedi
-              </Link>
+              {isUserAuthenticated ? (
+                // Contenuto del menù a tendina quando l'utente è autenticato
+                <>
+                  <Link to="/login" className="text-black">
+                    Profilo
+                  </Link>
+                  <Link to="/" className="text-black">
+                    Il mio piano
+                  </Link>
+                  <Link to="/" className="text-black">
+                    Il mio storico
+                  </Link>
+                  <Link onClick={handleLogout}>
+                    <span className="text-black">Esci</span>
+                  </Link>
+                </>
+              ) : (
+                // Contenuto del menù a tendina quando l'utente non è autenticato
+                <Link to="/login" className="text-black">
+                  Accedi
+                </Link>
+              )}
             </div>
           )}
         </li>
