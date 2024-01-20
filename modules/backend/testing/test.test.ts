882072c085530c6d634a2f3bd6a69e121c896803
Giuseppe Raiola Paduano <g.raiolapaduano@gmail.com>
2024-01-20 10:25:49 +0100

    build(backend): aggiungo dipendenze per testing di unità

---
+++ modules/backend/testing/test.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('Test', () => {
+  it('Test per configurare mocha', () => {
+    expect(true).to.be.true;
+  });
+});
