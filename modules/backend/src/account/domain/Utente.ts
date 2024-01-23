1feb0dda490706ac452ff49cd2b01f48d91b4bcd
ldamato17 <l.damato17@studenti.unisa.it>
2024-01-24 00:36:01 +0100

    docs: documentazione codice sorgente
    Co-authored-by: RaffyAS99 <RaffyAS99@users.noreply.github.com>
    Co-authored-by: raff002 <raff002@users.noreply.github.com>
    Co-authored-by: Antonio D'Amato <Fatted@users.noreply.github.com>
    Co-authored-by: MegaPepeMan <MegaPepeMan@users.noreply.github.com>

--- modules/backend/src/account/domain/Utente.ts
+++ modules/backend/src/account/domain/Utente.ts
@@ -21,44 +21,82 @@ class Utente {
 
   private password: string;
 
-  // creo i metodi get
-  public getIdUtente(): number {
+   /**
+   * Ottiene l'identificatore dell'utente.
+   * @returns L'identificatore dell'utente.
+   */
+   public getIdUtente(): number {
     return this.idUtente;
   }
 
+  /**
+   * Ottiene il nome dell'utente.
+   * @returns Il nome dell'utente.
+   */
   public getNome(): string {
     return this.nome;
   }
 
+  /**
+   * Ottiene il cognome dell'utente.
+   * @returns Il cognome dell'utente.
+   */
   public getCognome(): string {
     return this.cognome;
   }
 
+  /**
+   * Ottiene l'email dell'utente.
+   * @returns L'email dell'utente.
+   */
   public getEmail(): string {
     return this.email;
   }
 
+  /**
+   * Ottiene la password dell'utente.
+   * @returns La password dell'utente.
+   */
   public getPassword(): string {
     return this.password;
   }
 
-  // creo i metodi set
+  /**
+   * Imposta l'identificatore dell'utente.
+   * @param idUtente - Il nuovo identificatore dell'utente.
+   */
   public setIdUtente(idUtente: number): void {
     this.idUtente = idUtente;
   }
 
+  /**
+   * Imposta il nome dell'utente.
+   * @param nome - Il nuovo nome dell'utente.
+   */
   public setNome(nome: string): void {
     this.nome = nome;
   }
 
+  /**
+   * Imposta il cognome dell'utente.
+   * @param cognome - Il nuovo cognome dell'utente.
+   */
   public setCognome(cognome: string): void {
     this.cognome = cognome;
   }
 
+  /**
+   * Imposta l'email dell'utente.
+   * @param email - La nuova email dell'utente.
+   */
   public setEmail(email: string): void {
     this.email = email;
   }
 
+  /**
+   * Imposta la password dell'utente.
+   * @param password - La nuova password dell'utente.
+   */
   public setPassword(password: string): void {
     this.password = password;
   }
--- modules/backend/src/piano/service/ServiziPreventivoImpl.ts
+++ modules/backend/src/piano/service/ServiziPreventivoImpl.ts
@@ -1,6 +1,10 @@
 import PreventivoDAO from '../../DAO/PreventivoDAO';
 
 class ServiziPreventivoImpl {
+  /**
+ * Metodo statico che restituisce tutti i preventivi.
+ * @returns Un array di preventivi o `null` se non sono disponibili.
+ */
   static async TuttiPreventivi() {
     // richiamo il metodo TuttiPreventivi del PreventivoDAO
     const preventivi = await PreventivoDAO.getPreventivi();
@@ -10,7 +14,13 @@ class ServiziPreventivoImpl {
     return null;
   }
 
-  // metodo che mi consente di creare un preventivo
+ /**
+ * Metodo statico che crea un nuovo preventivo.
+ * @param idUtente - L'identificativo dell'utente associato al preventivo.
+ * @param limitiAddestramenti - I limiti di addestramenti associati al preventivo.
+ * @param limitiSalvataggi - I limiti di salvataggi associati al preventivo.
+ * @returns Il preventivo creato o `null` in caso di errore.
+ */
   static async creaPreventivo(
     idUtente: number,
     limitiAddestramenti: number,
@@ -28,7 +38,11 @@ class ServiziPreventivoImpl {
     return null;
   }
 
-  // metodo che mi consente di eliminare un preventivo
+  /**
+ * Metodo statico che elimina un preventivo.
+ * @param idUtente - L'identificativo dell'utente associato al preventivo.
+ * @returns Il preventivo eliminato o `null` in caso di errore.
+ */
   static async eliminaPreventivo(idUtente: number) {
     // richiamo il metodo eliminaPreventivo del PreventivoDAO
     const preventivoElim = await PreventivoDAO.eliminaPreventivo(idUtente);
@@ -38,7 +52,11 @@ class ServiziPreventivoImpl {
     return null;
   }
 
-  // metodo che mi consente di recuperare un preventivo
+  /**
+ * Metodo statico che restituisce un preventivo.
+ * @param idUtente - L'identificativo dell'utente associato al preventivo.
+ * @returns Il preventivo richiesto o `null` se non disponibile.
+ */
   static async getPreventivo(idUtente: number) {
     // richiamo il metodo getPreventivo del PreventivoDAO
     const preventivo = await PreventivoDAO.getPreventivo(idUtente);
@@ -48,7 +66,11 @@ class ServiziPreventivoImpl {
     return null;
   }
 
-  // metodo che mi consente di recuperare l'id di un preventivo
+  /**
+ * Metodo statico che restituisce l'identificativo di un preventivo.
+ * @param idUtente - L'identificativo dell'utente associato al preventivo.
+ * @returns L'identificativo del preventivo o `null` se non disponibile.
+ */
   static async getIdPreventivo(idUtente: number) {
     // richiamo il metodo getIdPreventivo del PreventivoDAO
     const preventivo = await PreventivoDAO.getIdPreventivo(idUtente);
@@ -58,7 +80,11 @@ class ServiziPreventivoImpl {
     return null;
   }
 
-  // metodo che mi consente di recuperare i limiti di addestramento di un preventivo
+  /**
+ * Metodo statico che restituisce i limiti di addestramento di un preventivo.
+ * @param idUtente - L'identificativo dell'utente associato al preventivo.
+ * @returns I limiti di addestramento richiesti o `null` se non disponibili.
+ */
   static async getLimitiAddestramenti(idUtente: number) {
     // richiamo il metodo getLimitiAddestramento del PreventivoDAO
     const preventivo = await PreventivoDAO.getLimitiAddestramenti(idUtente);
@@ -68,7 +94,11 @@ class ServiziPreventivoImpl {
     return null;
   }
 
-  // metodo che mi consente di recuperare i limiti di salvataggio di un preventivo
+  /**
+ * Metodo statico che restituisce i limiti di salvataggio di un preventivo.
+ * @param idUtente - L'identificativo dell'utente associato al preventivo.
+ * @returns I limiti di salvataggio richiesti o `null` se non disponibili.
+ */
   static async getLimitiSalvataggi(idUtente: number) {
     // richiamo il metodo getLimitiSalvataggi del PreventivoDAO
     const preventivo = await PreventivoDAO.getLimitiSalvataggi(idUtente);
@@ -78,7 +108,11 @@ class ServiziPreventivoImpl {
     return null;
   }
 
-  // metodo che mi consente di recuperare il prezzo di un preventivo
+  /**
+ * Metodo statico che restituisce il prezzo di un preventivo.
+ * @param idUtente - L'identificativo dell'utente associato al preventivo.
+ * @returns Il prezzo richiesto o `null` se non disponibile.
+ */
   static async getPrezzo(idUtente: number) {
     // richiamo il metodo getPrezzo del PreventivoDAO
     const preventivo = await PreventivoDAO.getPrezzo(idUtente);
@@ -88,7 +122,11 @@ class ServiziPreventivoImpl {
     return null;
   }
 
-  // metodo che mi consente di recuperare lo stato di un preventivo
+  /**
+ * Metodo statico che restituisce lo stato di un preventivo.
+ * @param idUtente - L'identificativo dell'utente associato al preventivo.
+ * @returns Lo stato del preventivo o `null` se non disponibile.
+ */
   static async getStato(idUtente: number) {
     // richiamo il metodo getStato del PreventivoDAO
     const preventivo = await PreventivoDAO.getStato(idUtente);
@@ -98,7 +136,12 @@ class ServiziPreventivoImpl {
     return null;
   }
 
-  // metodo che mi consente di aggiornare lo stato di un preventivo
+  /**
+ * Metodo statico che aggiorna lo stato di un preventivo.
+ * @param idPreventivo - L'identificativo del preventivo da aggiornare.
+ * @param stato - Il nuovo stato da assegnare al preventivo.
+ * @returns Il preventivo aggiornato o `null` in caso di errore.
+ */
   static async aggiornaStato(idPreventivo: number, stato: string) {
     // richiamo il metodo setStato del PreventivoDAO
     const preventivo = await PreventivoDAO.setStato(idPreventivo, stato);
@@ -108,7 +151,12 @@ class ServiziPreventivoImpl {
     return null;
   }
 
-  // metodo che mi consente di aggiornare il prezzo di un preventivo
+  /**
+ * Metodo statico che aggiorna il prezzo di un preventivo.
+ * @param idPreventivo - L'identificativo del preventivo da aggiornare.
+ * @param prezzo - Il nuovo prezzo da assegnare al preventivo.
+ * @returns Il preventivo aggiornato o `null` in caso di errore.
+ */
   static async aggiornaPrezzo(idPreventivo: number, prezzo: number) {
     // richiamo il metodo setPrezzo del PreventivoDAO
     const preventivo = await PreventivoDAO.setPrezzo(idPreventivo, prezzo);
@@ -117,13 +165,22 @@ class ServiziPreventivoImpl {
     }
     return null;
   }
-
+  /**
+ * Metodo statico che elimina un preventivo per identificativo.
+ * @param idPreventivo - L'identificativo del preventivo da eliminare.
+ */
   static async eliminaPreventivoById(idPreventivo: number) {
     // richiamo il metodo elminiaPreventivo del PreventivoDAO
     await PreventivoDAO.eliminaPreventivoById(idPreventivo);
   }
 
-  // metodo che mi consente di modificare un preventivo
+ /**
+ * Metodo statico che modifica un preventivo.
+ * @param stato - Il nuovo stato da assegnare al preventivo.
+ * @param prezzo - Il nuovo prezzo da assegnare al preventivo.
+ * @param idPreventivo - L'identificativo del preventivo da modificare.
+ * @returns Il preventivo modificato o `null` in caso di errore.
+ */
   static async ModificaPreventivo(
     stato: string,
     prezzo: number,
